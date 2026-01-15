0eaba72b8746a913036bea417a66044bff67e15f
Nowy:3 <egesdabest@gmail.com>
2026-01-15 04:42:35 +0300

    Create MvS.lua

---
+++ misc/MvS.lua
@@ -0,0 +1,5 @@
+-- HITBOX EXPANDER SCRIPT FOR: https://www.roblox.com/games/12355337193
+getgenv().size = Vector3.new(10, 10, 10)
+getgenv().color = Color3.fromRGB(255, 0, 0)
+getgenv().transparency = 0.5
+loadstring(game:HttpGet("https://pastebin.com/raw/gn58vyj7"))()

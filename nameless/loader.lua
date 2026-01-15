f343d88135a68923465ab33fcf6aaa79fbfbfb4c
Nowy:3 <egesdabest@gmail.com>
2026-01-15 06:55:47 +0300

    Update loader.lua

--- nameless/loader.lua
+++ nameless/loader.lua
@@ -1,1 +1,5 @@
-soon
+if game.PlaceId==71895508397153 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotnowy/shitscripts/refs/heads/main/nameless/scripts/dod.lua"))()
+else
+warn("not supported")
+end

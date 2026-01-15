84b22e2b8026df209801249e0a56b5f6262149f0
Nowy:3 <egesdabest@gmail.com>
2026-01-15 06:57:53 +0300

    Update loader.lua

--- nameless/loader.lua
+++ nameless/loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId==71895508397153 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotnowy/shitscripts/refs/heads/main/nameless/scripts/dod.lua"))()
 else
-warn("not supported")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))():InitNotifications():Notify("This game isn't supported yet. If this game is popular please add in suggestions in our discord server.",10,"information")
 end
